d62baed34ccbeb6c157164fcc4d670a0b649ac27
Roman Tkachyov <tkachyovroman@gmail.com>
2022-03-15 21:47:51 +0300

    add empty task

--- src/tkachyov/lesson3/Task1.java
+++ src/tkachyov/lesson3/Task1.java
@@ -2,6 +2,39 @@ package tkachyov.lesson3;
 
 public class Task1 {
     public static void main(String[] args) {
+        int value = -123;
+        String valueType;
+
+        if (value > 0) {
+            valueType = value + " - положительное число";
+        } else {
+            valueType = value + " - отрицательное число";
+        }
+        int count = 0;
+
+        for (int i = value; i != 0; count++) {
+            i /= 10;
+        }
+
+        switch (count) {
+            case 1: {
+                System.out.println(valueType + ", " + "однозначное число.");
+                break;
+            }
+            case 2: {
+                System.out.println(valueType + ", " + "двухзначное число число.");
+                break;
+            }
+            case 3: {
+                System.out.println(valueType + ", " + "трехзначное число.");
+                break;
+            }
+            default: {
+                System.out.println(value + " - ноль.");
+                break;
+            }
+        }
+
 
     }
 }
